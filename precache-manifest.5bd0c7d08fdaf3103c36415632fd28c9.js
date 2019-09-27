22e005ac0a8d12f5274442cc6e7ce7dbfc6ef8fc
drest <drestbm@gmail.com>
2019-09-28 01:52:07 +0700

    Updates

---
+++ precache-manifest.5bd0c7d08fdaf3103c36415632fd28c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e127d41f21a6f0ec57064079d01dd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c26442b090473214604",
+    "url": "/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "0e2185b90c6a43887803",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "6c26442b090473214604",
+    "url": "/static/js/2.9f6d863a.chunk.js"
+  },
+  {
+    "revision": "0e2185b90c6a43887803",
+    "url": "/static/js/main.7ebe4148.chunk.js"
+  },
+  {
+    "revision": "10864ecf5e3bd07fddcd",
+    "url": "/static/js/runtime-main.1f90b763.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
