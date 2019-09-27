bc3e3b0c69befabe61adf059cb23eecea18b9d4c
drest <drestbm@gmail.com>
2019-09-28 01:54:34 +0700

    Updates

---
+++ precache-manifest.e6f8b641999ee81c0ed6d61b91017f16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879264c57f30ec64cf65e768f099594a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ece735891524e98746cf",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "0e2185b90c6a43887803",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "ece735891524e98746cf",
+    "url": "./static/js/2.a3413feb.chunk.js"
+  },
+  {
+    "revision": "0e2185b90c6a43887803",
+    "url": "./static/js/main.7ebe4148.chunk.js"
+  },
+  {
+    "revision": "830435a959e7d84d4ba8",
+    "url": "./static/js/runtime-main.0c5076f4.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
